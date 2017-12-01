0c3ca964a09ebc566d28432259cc92f7cd789544
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-01 12:31:32 +0530

    spinner incomplete

--- app/src/main/java/com/example/sidkathuria14/speech/MainActivity.java
+++ app/src/main/java/com/example/sidkathuria14/speech/MainActivity.java
@@ -7,20 +7,49 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.sql.Array;
 
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener{
     private Synthesizer m_syn;EditText etInput;String input;
+    Spinner language,voice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etInput = (EditText)findViewById(R.id.etinput);
+        language = (Spinner)findViewById(R.id.languageSpinner);
+        voice = (Spinner)findViewById(R.id.voiceSpinner);
+
+
+        String [] lang = new String[]{
+                "fr-FR","fr-CA","fi-FI","es-MX","hi-IN","en-US","es-ES","pl-PL"
+        };
+        String [] voic = new String[]{
+                "male","female"
+        };
+        ArrayAdapter<String> dataAdapterVoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, voic);
+
+
+        dataAdapterVoice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ArrayAdapter<String> dataAdapterLanguage = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lang);
+
+
+        dataAdapterVoice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        language.setAdapter(dataAdapterLanguage);
+        voice.setAdapter(dataAdapterVoice);
+
+
+
 
 
         if (getString(R.string.api_key).startsWith("Please")) {
@@ -40,7 +69,7 @@ public class MainActivity extends ActionBarActivity {
 
             m_syn.SetServiceStrategy(com.example.sidkathuria14.speech.Synthesizer.ServiceStrategy.AlwaysService);
 
-            Voice v = new Voice("en-US", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)", Voice.Gender.Female, true);
+            Voice v = new Voice("hi-IN", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)", Voice.Gender.Male, true);
             //Voice v = new Voice("zh-CN", "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)", Voice.Gender.Female, true);
             m_syn.SetVoice(v, null);
 
@@ -69,5 +98,16 @@ public class MainActivity extends ActionBarActivity {
                 }
             });
         }
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
     }
 }
