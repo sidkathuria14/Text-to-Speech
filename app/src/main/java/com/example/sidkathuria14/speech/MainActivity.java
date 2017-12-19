834df2868232f89e476db5ec140cd560713831c3
siddharth kathuria <kathuria.siddharth@yahoo.in>
2017-12-19 18:33:39 +0530

    few bugs removed

--- app/src/main/java/com/example/sidkathuria14/speech/MainActivity.java
+++ app/src/main/java/com/example/sidkathuria14/speech/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.sidkathuria14.speech;
 
 import android.app.Activity;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -16,17 +20,21 @@ import android.widget.Toast;
 import java.sql.Array;
 
 
-public class MainActivity extends ActionBarActivity implements AdapterView.OnItemSelectedListener{
-    private Synthesizer m_syn;EditText etInput;String input;
-    Spinner language,voice;
+
+public class MainActivity extends Activity implements AdapterView.OnItemSelectedListener{
+    private Synthesizer m_syn_male,m_syn_female;EditText etInput;String input;
+   public String item = "male";
+    Spinner language_spinner,voice_spinner;
+    String voice = "male";
+    public static final String TAG = "speech";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etInput = (EditText)findViewById(R.id.etinput);
-        language = (Spinner)findViewById(R.id.languageSpinner);
-        voice = (Spinner)findViewById(R.id.voiceSpinner);
+        language_spinner = (Spinner)findViewById(R.id.languageSpinner);
+        voice_spinner = (Spinner)findViewById(R.id.voiceSpinner);
 
 
         String [] lang = new String[]{
@@ -45,13 +53,49 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         dataAdapterVoice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
 
-        language.setAdapter(dataAdapterLanguage);
-        voice.setAdapter(dataAdapterVoice);
+        language_spinner.setAdapter(dataAdapterLanguage);
+        voice_spinner.setAdapter(dataAdapterVoice);
+
+
+        findViewById(R.id.stop_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "stoppped", Toast.LENGTH_SHORT).show();
+               if(item.equals("male")) {
+                   male_voice();
+                   m_syn_male.stopSound();
+               }
+               else if(item.equals("female")){
+                   female_voice();
+                   m_syn_female.stopSound();
+               }
+            }
+        });
+
+        findViewById(R.id.play_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "playing!", Toast.LENGTH_SHORT).show();
+                input = etInput.getText().toString();
+                if(item.equals("male")){
+                    Log.d(TAG, "onClick: "+ "play button - male");
+                    male_voice();
+                    m_syn_male.SpeakToAudio(input);
+                }
+                else if(item.equals("female")){
+                    Log.d(TAG, "onClick: "+ "play button - female");
+                    female_voice();
+                    m_syn_female.SpeakToAudio(input);
+                }
+
+            }
+        });
 
 
 
-
-
+    }
+    public void male_voice(){
+        Log.d(TAG, "male_voice: ");
         if (getString(R.string.api_key).startsWith("Please")) {
             new AlertDialog.Builder(this)
                     .setTitle(getString(R.string.add_subscription_key_tip_title))
@@ -60,54 +104,121 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
                     .show();
         } else {
 
-            if (m_syn == null) {
+            if (m_syn_male == null) {
                 // Create Text To Speech Synthesizer.
-                m_syn = new com.example.sidkathuria14.speech.Synthesizer(getString(R.string.api_key));
+                m_syn_male = new com.example.sidkathuria14.speech.Synthesizer(getString(R.string.api_key));
             }
 
             Toast.makeText(this, "If the wave is not played, please see the log for more information.", Toast.LENGTH_LONG).show();
 
-            m_syn.SetServiceStrategy(com.example.sidkathuria14.speech.Synthesizer.ServiceStrategy.AlwaysService);
+            m_syn_male.SetServiceStrategy(com.example.sidkathuria14.speech.Synthesizer.ServiceStrategy.AlwaysService);
 
             Voice v = new Voice("hi-IN", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)", Voice.Gender.Male, true);
             //Voice v = new Voice("zh-CN", "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)", Voice.Gender.Female, true);
-            m_syn.SetVoice(v, null);
+            m_syn_male.SetVoice(v, null);
 
             // Use a string for speech.
-            m_syn.SpeakToAudio("Greetings, user!");
+//            m_syn_male.SpeakToAudio("Greetings, user!");
             //getString(R.string.tts_text)
 
             // Use SSML for speech.
             String text = "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xml:lang=\"en-US\"><voice xml:lang=\"en-US\" name=\"Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)\">You can also use SSML markup for text to speech.</voice></speak>";
 //            m_syn.SpeakSSMLToAudio(text);
 
-            findViewById(R.id.stop_btn).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "stoppped", Toast.LENGTH_SHORT).show();
-                    m_syn.stopSound();
-                }
-            });
 
-            findViewById(R.id.play_btn).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(MainActivity.this, "playing!", Toast.LENGTH_SHORT).show();
-                    input = etInput.getText().toString();
-                    m_syn.SpeakToAudio(input);
-                }
-            });
         }
     }
+    public void female_voice(){
+        Log.d(TAG, "female_voice: ");
+        if (getString(R.string.api_key).startsWith("Please")) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.add_subscription_key_tip_title))
+                    .setMessage(getString(R.string.add_subscription_key_tip))
+                    .setCancelable(false)
+                    .show();
+        } else {
+
+            if (m_syn_female == null) {
+                // Create Text To Speech Synthesizer.
+                m_syn_female = new com.example.sidkathuria14.speech.Synthesizer(getString(R.string.api_key));
+            }
+
+            Toast.makeText(this, "If the wave is not played, please see the log for more information.", Toast.LENGTH_LONG).show();
+
+            m_syn_female.SetServiceStrategy(com.example.sidkathuria14.speech.Synthesizer.ServiceStrategy.AlwaysService);
+
+            Voice v = new Voice("hi-IN", "Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)", Voice.Gender.Female, true);
+            //Voice v = new Voice("zh-CN", "Microsoft Server Speech Text to Speech Voice (zh-CN, HuihuiRUS)", Voice.Gender.Female, true);
+            m_syn_female.SetVoice(v, null);
+
+            // Use a string for speech.
+//            m_syn_female.SpeakToAudio("Greetings, user!");
+            //getString(R.string.tts_text)
+
+            // Use SSML for speech.
+            String text = "<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xmlns:mstts=\"http://www.w3.org/2001/mstts\" xml:lang=\"en-US\"><voice xml:lang=\"en-US\" name=\"Microsoft Server Speech Text to Speech Voice (en-US, ZiraRUS)\">You can also use SSML markup for text to speech.</voice></speak>";
+//            m_syn.SpeakSSMLToAudio(text);
+
+
+        }
+    }
+    //spinner
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         // On selecting a spinner item
-        String item = parent.getItemAtPosition(position).toString();
+
+
+
+        Spinner spinner = (Spinner) parent;
+        if(spinner.getId() == R.id.voiceSpinner)
+        {
+            //do this
+            item = parent.getItemAtPosition(position).toString();
+
+            Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+            if(item.equals("male")){
+                Log.d(TAG, "onItemSelected: male");
+            }
+            else if(item.equals("female")){
+                Log.d(TAG, "onItemSelected: female");
+            }
+        }
+        else if(spinner.getId() == R.id.languageSpinner)
+        {
+            //do this
+        }
+
 
         // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+
     }
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId() == R.id.stt){
+startActivity(new Intent(MainActivity.this,stt.class));
+        }
+        if(item.getItemId() == R.id.tts){
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public MenuInflater getMenuInflater() {
+        return super.getMenuInflater();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.speech,menu);
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
